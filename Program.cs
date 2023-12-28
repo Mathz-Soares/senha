699956f616c365a573f46b89bd0e1adab407be87
Mathz-Soares <matheussou487@gmail.com>
2023-12-28 17:34:24 -0300

    senha

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Senha valida ou invalida
+string snh = "1234";
+Console.WriteLine("Digite a senha de números do usuário");
+string cdg = Console.ReadLine();
+
+if (cdg == snh)
+{
+    Console.WriteLine("senha valida");
+}
+else 
+{
+    Console.WriteLine("senha invalida");
+}
